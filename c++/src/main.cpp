ea79511c3e6aa801b71e8634f25f8aab5d924da6
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-03-09 16:27:19 +0100

    default main

--- c++/src/main.cpp
+++ c++/src/main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
 
+#include "program.hpp"
+
+Program *program = nullptr;
+
+static int WIDTH = 1000;
+static int HEIGHT = 1000;
+
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    const float FPS = 60;
+    const float frameDelay = 1000.0f / FPS;
+
+    Uint32 frameStart;
+    int frameTime;
+
+    program = new Program();
+    program->init("CUDA METABALLS", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, false);
+    while(program->running())
+    {
+        frameStart = SDL_GetTicks();
+
+        program->handleEvents();
+        program->update();
+        program->render();
+
+        frameTime = SDL_GetTicks() - frameStart;
+        if (frameDelay > frameTime)
+        {
+            SDL_Delay(frameDelay - frameTime);
+        }
+        program->deltaTime = (float)((SDL_GetTicks() - frameStart) / 1000.0f);
+    }
+
+    program->clean();
+    free(program);
     return 0;
 }
